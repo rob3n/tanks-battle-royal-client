cd1d62310434a24b93bacb0251e0b0c3e7763b16
rob3n <roben1006@gmail.com>
2019-05-07 21:16:46 +0300

    refactoring scene, and add methods for create, reborn tank

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -5,20 +5,42 @@ import Tank from '../components/tank';
 export default class GameScene extends Phaser.Scene {
 	constructor() {
 		super('Game');
-		this.velocity = 0;
-		this.enemyVelocity = 0;
-		this.acceleration = 5;
-		this.enemyAcceleration = 5;
-		this.lastFired = +new Date();
-		this.lastFiredEnemy = +new Date();
-		this.isCollision = false;
 		this.map = null;
 		this.treesLayer = null;
 		this.cursors = null;
 		this.bullets = null;
-		this.bulletLifeTime = 4000;
+		this.rebornTankTime = 4000;
 		this.destroyTreeId = 57;
 		this.newKeys = null;
+
+		this.enemyConfig = {
+			scene: this,
+			key: 'tank_second',
+			x: 100,
+			y: 150,
+			texture: 'tank_red',
+			width: 38,
+			height: 46,
+			velocity: 0,
+			acceleration: 5
+		};
+		this.playerConfig = {
+			scene: this,
+			key: 'tank_first',
+			x: 350,
+			y: 350,
+			texture: 'tank_blue',
+			scale: 0.6,
+			width: 84,
+			height: 92,
+			velocity: 0,
+			acceleration: 5
+		};
+		this.defaultBulletConfig = {
+			defaultKey: 'bullet',
+			maxSize: 20,
+			runChildUpdate: true
+		};
 	}
 
 	preload() {
@@ -31,110 +53,21 @@ export default class GameScene extends Phaser.Scene {
 	}
 
 	create() {
+		// create map with tiles and collides
 		this.map = this.make.tilemap({ key: 'map' });
-		// set layers
 		const tileset = this.map.addTilesetImage('all_tiles', 'tiles');
-
 		this.background = this.map.createStaticLayer('BackLayer', tileset, 0, 0);
 		this.treesLayer = this.map.createDynamicLayer('TreesLayer', tileset, 0, 0);
-
 		this.treesLayer.setCollisionByProperty({ collides: true });
 
-		// collide trees and tank
-
-		this.player = new Tank({
-			scene: this,
-			key: 'tank_first',
-			x: 350,
-			y: 350,
-			texture: 'tank_blue',
-			scale: 0.6,
-			width: 84,
-			height: 92,
-			velocity: 0,
-			acceleration: 5
-		});
-
-		this.enemyPlayer = new Tank({
-			scene: this,
-			key: 'tank_second',
-			x: 100,
-			y: 150,
-			texture: 'tank_red',
-			width: 38,
-			height: 46,
-			velocity: 0,
-			acceleration: 5
-		});
-
-		this.physics.add.collider(this.player, this.enemy);
-		this.physics.add.collider(this.player, this.treesLayer);
-
-		this.physics.add.collider(this.enemyPlayer, this.enemy);
-		this.physics.add.collider(this.enemyPlayer, this.treesLayer);
-
-		this.physics.add.collider(this.enemyPlayer, this.player);
+		// create tanks with configs
+		this.createPlayer(this);
+		this.createEnemyPlayer(this);
 
 		// detect keyboard
 		this.newKeys = this.input.keyboard.addKeys('W,S,A,D,C');
-
 		this.cursors = this.input.keyboard.createCursorKeys();
 
-		// bullet
-		this.bullets = this.physics.add.group({
-			defaultKey: 'bullet',
-			maxSize: 20,
-			runChildUpdate: true
-		});
-
-		this.enemyBullets = this.physics.add.group({
-			defaultKey: 'bullet',
-			maxSize: 20,
-			runChildUpdate: true
-		});
-
-		// overlap
-		this.physics.add.collider(this.treesLayer, this.bullets, (bullet, tree) => {
-			bullet.destroy();
-			if (tree.index === this.destroyTreeId) {
-				this.map.removeTileAt(tree.x, tree.y);
-			}
-		});
-		this.physics.add.collider(
-			this.enemyPlayer,
-			this.bullets,
-			(bullet, enemy) => {
-				bullet.destroy();
-				enemy.destroy();
-			}
-		);
-		this.physics.add.collider(
-			this.enemyPlayer,
-			this.bullets,
-			(bullet, enemy) => {
-				bullet.destroy();
-				enemy.destroy();
-			}
-		);
-		this.physics.add.collider(
-			this.player,
-			this.enemyBullets,
-			(bullet, player) => {
-				bullet.destroy();
-				player.destroy();
-			}
-		);
-		this.physics.add.collider(
-			this.treesLayer,
-			this.enemyBullets,
-			(bullet, tree) => {
-				bullet.destroy();
-				// destroy tree with id
-				if (tree.index === this.destroyTreeId) {
-					this.map.removeTileAt(tree.x, tree.y);
-				}
-			}
-		);
 		/**
 		 * Only for debug, remove later
 		 */
@@ -163,6 +96,66 @@ export default class GameScene extends Phaser.Scene {
 		this.player.update(playerKeys, this.bullets);
 		this.enemyPlayer.update(enemyPlayerKeys, this.enemyBullets);
 		this.player.destroyBullets(this.bullets);
-		this.player.destroyBullets(this.enemyBullets);
+		this.enemyPlayer.destroyBullets(this.enemyBullets);
+	}
+
+	createPlayer(scene) {
+		this.player = new Tank(this.playerConfig);
+		this.enemyBullets = this.physics.add.group(this.defaultBulletConfig);
+		this.physics.add.collider(this.player, this.treesLayer);
+		this.physics.add.collider(this.player, this.enemyPlayer);
+		this.physics.add.collider(
+			this.player,
+			this.enemyBullets,
+			(bullet, enemy) => {
+				bullet.destroy();
+				enemy.destroy();
+
+				setTimeout(() => {
+					this.createPlayer(scene);
+				}, this.rebornTankTime);
+			}
+		);
+		this.physics.add.collider(
+			scene.treesLayer,
+			this.enemyBullets,
+			(bullet, tree) => {
+				bullet.destroy();
+				// destroy tree with id
+				if (tree.index === this.destroyTreeId) {
+					scene.map.removeTileAt(tree.x, tree.y);
+				}
+			}
+		);
+	}
+
+	createEnemyPlayer(scene) {
+		this.enemyPlayer = new Tank(this.enemyConfig);
+		this.bullets = this.physics.add.group(this.defaultBulletConfig);
+		this.physics.add.collider(this.enemyPlayer, this.treesLayer);
+		this.physics.add.collider(this.enemyPlayer, this.player);
+		this.physics.add.collider(
+			this.enemyPlayer,
+			this.bullets,
+			(bullet, enemy) => {
+				bullet.destroy();
+				enemy.destroy();
+
+				setTimeout(() => {
+					this.createEnemyPlayer(scene);
+				}, this.rebornTankTime);
+			}
+		);
+		this.physics.add.collider(
+			scene.treesLayer,
+			this.bullets,
+			(bullet, tree) => {
+				bullet.destroy();
+				// destroy tree with id
+				if (tree.index === this.destroyTreeId) {
+					scene.map.removeTileAt(tree.x, tree.y);
+				}
+			}
+		);
 	}
 }
