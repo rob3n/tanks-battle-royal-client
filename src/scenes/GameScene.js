b491da9f9e0ac4ad46f08f6f40e994b3f2596fcb
rob3n <roben1006@gmail.com>
2019-05-07 19:19:29 +0300

    update scene, bullets its group

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,9 +1,5 @@
 import Phaser from 'phaser';
-import Bullets from '../components/bullets';
 import Tank from '../components/tank';
-
-const TANK_MAX_VELOCITY = 100;
-const SHOOTING_TIMEOUT_MS = 500;
 
 // TODO: refactoring, create tank and bullets component
 export default class GameScene extends Phaser.Scene {
@@ -19,11 +15,7 @@ export default class GameScene extends Phaser.Scene {
 		this.map = null;
 		this.treesLayer = null;
 		this.cursors = null;
-		this.bullets = new Bullets({
-			defaultKey: 'bullet',
-			maxSize: 20,
-			runChildUpdate: true
-		});
+		this.bullets = null;
 		this.bulletLifeTime = 4000;
 		this.destroyTreeId = 57;
 		this.newKeys = null;
@@ -39,11 +31,6 @@ export default class GameScene extends Phaser.Scene {
 	}
 
 	create() {
-		// this.newKeys.W.on('down', () => {
-		// 	if (this.enemyVelocity < TANK_MAX_VELOCITY) {
-		// 		this.enemyVelocity += this.enemyAcceleration;
-		// 	}
-		// });
 		this.map = this.make.tilemap({ key: 'map' });
 		// set layers
 		const tileset = this.map.addTilesetImage('all_tiles', 'tiles');
@@ -86,6 +73,8 @@ export default class GameScene extends Phaser.Scene {
 		this.physics.add.collider(this.enemyPlayer, this.enemy);
 		this.physics.add.collider(this.enemyPlayer, this.treesLayer);
 
+		this.physics.add.collider(this.enemyPlayer, this.player);
+
 		// detect keyboard
 		this.newKeys = this.input.keyboard.addKeys('W,S,A,D,C');
 
@@ -105,16 +94,36 @@ export default class GameScene extends Phaser.Scene {
 		});
 
 		// overlap
-
-		// this.physics.add.overlap(this.enemy, this.bullets, this.destroyBullet);
 		this.physics.add.collider(this.treesLayer, this.bullets, (bullet, tree) => {
 			bullet.destroy();
-			// destroy tree with id
 			if (tree.index === this.destroyTreeId) {
 				this.map.removeTileAt(tree.x, tree.y);
 			}
 		});
-
+		this.physics.add.collider(
+			this.enemyPlayer,
+			this.bullets,
+			(bullet, enemy) => {
+				bullet.destroy();
+				enemy.destroy();
+			}
+		);
+		this.physics.add.collider(
+			this.enemyPlayer,
+			this.bullets,
+			(bullet, enemy) => {
+				bullet.destroy();
+				enemy.destroy();
+			}
+		);
+		this.physics.add.collider(
+			this.player,
+			this.enemyBullets,
+			(bullet, player) => {
+				bullet.destroy();
+				player.destroy();
+			}
+		);
 		this.physics.add.collider(
 			this.treesLayer,
 			this.enemyBullets,
@@ -132,13 +141,9 @@ export default class GameScene extends Phaser.Scene {
 		// console.log(this.tank);
 		// console.log(this.physics);
 		// console.log(this);
-		console.log(this.newKeys);
 	}
 
-	update(time, delta) {
-		this.fire();
-		this.destroyBullets();
-		this.destroyEnemyBullets();
+	update() {
 		const playerKeys = {
 			left: this.cursors.left,
 			right: this.cursors.right,
@@ -155,109 +160,9 @@ export default class GameScene extends Phaser.Scene {
 			fire: this.newKeys.C
 		};
 
-		this.player.update(playerKeys, time, delta);
-		this.enemyPlayer.update(enemyPlayerKeys, time, delta);
-	}
-
-	fire() {
-		const bullet = this.bullets.create(300, 400);
-
-		// if (bullet) {
-		// 	bullet.setAngle(this.tank.angle);
-		// 	bullet.setVelocity(
-		// 		300 * Math.cos((this.tank.angle - 90) * 0.01745),
-		// 		300 * Math.sin((this.tank.angle - 90) * 0.01745)
-		// 	);
-		// }
-	}
-
-	destroyBullets() {
-		const bullets = this.bullets.children.entries;
-		if (bullets.length) {
-			for (let i = 0; i < bullets.length; i += 1) {
-				const bullet = bullets[i];
-
-				if (bullet.y <= 0) {
-					bullet.destroy();
-				}
-
-				if (bullet.y >= this.cameras.main.height) {
-					bullet.destroy();
-				}
-
-				if (bullet.x <= 0) {
-					bullet.destroy();
-				}
-			}
-		}
-	}
-
-	fireEnemy() {
-		const bullet = this.enemyBullets.create(this.enemy.x, this.enemy.y);
-
-		if (bullet) {
-			bullet.setAngle(this.enemy.angle);
-			bullet.setVelocity(
-				BULLET_SPEED * Math.cos((this.enemy.angle - 90) * 0.01745),
-				BULLET_SPEED * Math.sin((this.enemy.angle - 90) * 0.01745)
-			);
-		}
-	}
-
-	destroyEnemyBullets() {
-		const bullets = this.enemyBullets.children.entries;
-		if (bullets.length) {
-			for (let i = 0; i < bullets.length; i += 1) {
-				const bullet = bullets[i];
-
-				if (bullet.y <= 0) {
-					bullet.destroy();
-				}
-
-				if (bullet.y >= this.cameras.main.height) {
-					bullet.destroy();
-				}
-
-				if (bullet.x <= 0) {
-					bullet.destroy();
-				}
-			}
-		}
-	}
-
-	rebornEnemy() {
-		this.enemy = this.physics.add.sprite(350, 350, 'tank_red');
-		this.enemy.setCollideWorldBounds(true);
-		this.enemy.setMaxVelocity(TANK_MAX_VELOCITY, TANK_MAX_VELOCITY);
-		this.enemy.setAngle(this.angle);
-		this.physics.add.collider(this.enemy, this.tank);
-		this.physics.add.collider(this.enemy, this.treesLayer);
-		this.physics.add.collider(this.enemy, this.bullets, (tank, bullet) => {
-			tank.destroy();
-			bullet.destroy();
-
-			setTimeout(() => {
-				this.rebornEnemy();
-			}, 3000);
-		});
-	}
-
-	reborn() {
-		this.tank = this.physics.add.sprite(230, 350, 'tank_blue');
-		this.tank.setCollideWorldBounds(true);
-		this.tank.scaleX = 0.6;
-		this.tank.scaleY = 0.6;
-		this.tank.setMaxVelocity(TANK_MAX_VELOCITY, TANK_MAX_VELOCITY);
-		this.tank.setAngle(this.angle);
-		this.physics.add.collider(this.tank, this.enemy);
-		this.physics.add.collider(this.treesLayer, this.tank);
-		this.physics.add.collider(this.tank, this.enemyBullets, (tank, bullet) => {
-			tank.destroy();
-			bullet.destroy();
-
-			setTimeout(() => {
-				this.reborn();
-			}, 3000);
-		});
+		this.player.update(playerKeys, this.bullets);
+		this.enemyPlayer.update(enemyPlayerKeys, this.enemyBullets);
+		this.player.destroyBullets(this.bullets);
+		this.player.destroyBullets(this.enemyBullets);
 	}
 }
